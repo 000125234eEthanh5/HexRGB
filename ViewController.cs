73e68f9bad4e4caa9ac7c363f17e14fb15052fcf
000125234eEthanh5 <32342571+000125234eEthanh5@users.noreply.github.com>
2017-10-23 13:26:45 -0600

    Initial commit

---
+++ ViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using UIKit;
+
+namespace HexRGB
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            ConvertButton.TouchUpInside += ConvertButton_TouchUpInside;
+        }
+
+        void ConvertButton_TouchUpInside(object sender, EventArgs e)
+        {
+            string hexValue = HexValueTextField.Text;
+            string RedHexValue = hexValue.Substring (0, 2);
+            string GreenHexValue = hexValue.Substring (2, 2);
+            string BlueHexValue = hexValue.Substring (4, 2);
+
+            int RedValue = int.Parse(RedHexValue, System.Globalization.NumberStyles.HexNumber);
+            int GreenValue = int.Parse(GreenHexValue, System.Globalization.NumberStyles.HexNumber);
+            int BlueValue = int.Parse(BlueHexValue, System.Globalization.NumberStyles.HexNumber);
+
+            RedValueLabel.Text = RedValue.ToString();
+            GreenValueLabel.Text = GreenValue.ToString();
+            BlueValueLabel.Text = BlueValue.ToString();
+
+            ColourView.BackgroundColor = UIColor.FromRGB(RedValue, GreenValue, BlueValue);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,73 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace HexRGB
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel BlueValueLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView ColourView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ConvertButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel GreenValueLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField HexValueTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel RedValueLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (BlueValueLabel != null) {
+                BlueValueLabel.Dispose ();
+                BlueValueLabel = null;
+            }
+
+            if (ColourView != null) {
+                ColourView.Dispose ();
+                ColourView = null;
+            }
+
+            if (ConvertButton != null) {
+                ConvertButton.Dispose ();
+                ConvertButton = null;
+            }
+
+            if (GreenValueLabel != null) {
+                GreenValueLabel.Dispose ();
+                GreenValueLabel = null;
+            }
+
+            if (HexValueTextField != null) {
+                HexValueTextField.Dispose ();
+                HexValueTextField = null;
+            }
+
+            if (RedValueLabel != null) {
+                RedValueLabel.Dispose ();
+                RedValueLabel = null;
+            }
+        }
+    }
+}
